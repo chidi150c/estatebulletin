d88841d5707f99a15dd165c561c20584b7c1191b
unknown <chidi150c@gmail.com>
2017-09-20 15:02:33 +0100

    office update

--- app/apphandler.go
+++ app/apphandler.go
@@ -23,16 +23,43 @@ func NewAppHandler() *AppHandler {
 		//Session: s,
 		Logger: log.New(os.Stderr, "", log.LstdFlags),
 	}
-	h.mux.Get("/app/index", h.indexHandler)
+	h.mux.Get("/app/1", h.indexHandler)
+	h.mux.Get("/app/2", h.socialHandler)
+	h.mux.Get("/app/3", h.apartmentHandler)
+	h.mux.Get("/app/4", h.hotelHandler)
+	h.mux.Get("/app/5", h.analyticHandler)
 	return h
 }
 func (a *AppHandler) indexHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("templates/index.html"))
+	if err := tmpl.Execute(w, nil); err != nil {
+		log.Fatal(err)
+	}
+}
+func (a *AppHandler) socialHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("templates/social.html"))
+	if err := tmpl.Execute(w, nil); err != nil {
+		log.Fatal(err)
+	}
+}
+func (a *AppHandler) apartmentHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("templates/apartment.html"))
+	if err := tmpl.Execute(w, nil); err != nil {
+		log.Fatal(err)
+	}
+}
+func (a *AppHandler) hotelHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("templates/hotel.html"))
+	if err := tmpl.Execute(w, nil); err != nil {
+		log.Fatal(err)
+	}
+}
+func (a *AppHandler) analyticHandler(w http.ResponseWriter, r *http.Request) {
 	tmpl := template.Must(template.ParseFiles("templates/analytic.html"))
 	if err := tmpl.Execute(w, nil); err != nil {
 		log.Fatal(err)
 	}
 }
-
 func (a *AppHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	a.mux.ServeHTTP(w, r)
 }
